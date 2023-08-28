e47dbc17e69e7b90409251afba3c9fb76bb24a03
Betpo5 <betzadiaz35@gmail.com>
2023-08-28 16:33:34 -0600

    Cambios de codigo en rama

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, este es otro cambio!");
+Console.WriteLine("Creo que solo funciona con cambios en el código....!");
