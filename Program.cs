fc3358f7a656fbeae32c07f27ff5806f1bd1d36c
davidgonzalez5 <137570208+davidgonzalez5@users.noreply.github.com>
2023-08-28 16:41:56 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("¿Todo bien?");
- Console.WriteLine("¿Todo bien?");
-Console.WriteLine("¿Todo bien?");
-Console.WriteLine("¿Todo bien?");
-Console.WriteLine("¿Todo bien?");
-Console.WriteLine("¿Todo bien?");
